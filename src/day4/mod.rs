1745bb82eb3385b3b02645127da39e2a94134375
Spencer Ferris <git@spencewenski.com>
2021-12-02 23:20:37 -0800

    Add part1/part2 methods for day 4

--- src/day4/mod.rs
+++ src/day4/mod.rs
@@ -3,10 +3,12 @@ use crate::io::reader;
 use anyhow::Result;
 use std::io::BufRead;
 
+type LineType = i64;
+
 pub fn day4(args: &Arguments) -> Result<()> {
     let reader = reader(args.src_file.as_ref())?;
 
-    let mut lines: Vec<i64> = Vec::new();
+    let mut lines: Vec<LineType> = Vec::new();
     for line in reader.lines() {
         let line = line?;
         let line = line.parse()?;
@@ -14,13 +16,21 @@ pub fn day4(args: &Arguments) -> Result<()> {
     }
     let lines = lines;
 
-    // let result = if args.part == 1 {
-    //     part1(lines)
-    // } else {
-    //     part2(lines)
-    // }?;
-    //
-    // info!("{:?}", result);
+    let result = if args.part == 1 {
+        part1(lines)
+    } else {
+        part2(lines)
+    }?;
+
+    info!("{:?}", result);
 
     Ok(())
+}
+
+fn part1(lines: Vec<LineType>) -> Result<i64> {
+    Ok(0)
+}
+
+fn part2(lines: Vec<LineType>) -> Result<i64> {
+    Ok(0)
 }

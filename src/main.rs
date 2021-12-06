b72441cce251cd6decc587c905df691c341a4817
Spencer Ferris <git@spencewenski.com>
2021-12-06 00:03:23 -0800

    Boilerplate and read input for day 6

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use advent_of_code_2021::day2::day2;
 use advent_of_code_2021::day3::day3;
 use advent_of_code_2021::day4::day4;
 use advent_of_code_2021::day5::day5;
+use advent_of_code_2021::day6::day6;
 use anyhow::Result;
 use dotenv::dotenv;
 use env_logger::Builder;
@@ -29,6 +30,7 @@ fn main() -> Result<()> {
         3 => day3(&args)?,
         4 => day4(&args)?,
         5 => day5(&args)?,
+        6 => day6(&args)?,
         _ => {
             return Err(anyhow::Error::msg(format!(
                 "Unrecognized day number: {}",

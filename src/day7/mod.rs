bf912a77caedd6f070120882eefb953459e2f264
Spencer Ferris <git@spencewenski.com>
2021-12-06 22:00:04 -0800

    Solution for day 7

--- src/day7/mod.rs
+++ src/day7/mod.rs
@@ -1,23 +1,24 @@
 use crate::arguments::Arguments;
 use crate::io::reader;
 use anyhow::Result;
+use std::cmp::{max, min};
 use std::io::BufRead;
 
 pub fn day7(args: &Arguments) -> Result<()> {
-    let reader = reader(args.src_file.as_ref())?;
+    let mut reader = reader(args.src_file.as_ref())?;
 
-    let mut numbers: Vec<u64> = Vec::new();
-    for line in reader.lines() {
-        let line = line?;
-        let num = line.parse()?;
-        numbers.push(num);
-    }
-    let numbers = numbers;
+    let mut line = String::new();
+    reader.read_line(&mut line)?;
+
+    let numbers = line.split(",").map(|s| s.trim().parse().unwrap()).collect();
 
     let result = if args.part == 1 {
-        part1(&numbers)
+        min_fuel(numbers, |a, b| max(a, b) - min(a, b))
     } else {
-        part2(&numbers)
+        min_fuel(numbers, |a, b| {
+            let distance = max(a, b) - min(a, b);
+            (distance * (distance + 1)) / 2
+        })
     }?;
 
     info!("{:?}", result);
@@ -25,10 +26,16 @@ pub fn day7(args: &Arguments) -> Result<()> {
     Ok(())
 }
 
-fn part1(numbers: &Vec<u64>) -> Result<usize> {
-    Ok(0)
-}
+fn min_fuel<F>(numbers: Vec<u64>, fuel_calculator: F) -> Result<u64>
+where
+    F: Fn(u64, u64) -> u64,
+{
+    let min_position = *numbers.iter().min().unwrap();
+    let max_position = *numbers.iter().max().unwrap();
 
-fn part2(numbers: &Vec<u64>) -> Result<usize> {
-    Ok(0)
+    let result = (min_position..=max_position)
+        .map(|to| numbers.iter().map(|from| fuel_calculator(*from, to)).sum())
+        .min()
+        .unwrap();
+    Ok(result)
 }

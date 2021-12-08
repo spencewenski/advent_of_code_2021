630fae5ff2dbe863b049dadb7c3cff23ad4ec513
Spencer Ferris <spencer@spencewenski.com>
2021-12-07 22:34:38 -0800

    Solution for day 8 part 1
    Not processing part 2 right now, will continue later

--- src/day8/mod.rs
+++ src/day8/mod.rs
@@ -1,20 +1,25 @@
 use crate::arguments::Arguments;
 use crate::io::reader;
 use anyhow::Result;
+use itertools::Itertools;
+use std::collections::HashSet;
 use std::io::BufRead;
 
 pub fn day8(args: &Arguments) -> Result<()> {
-    let mut reader = reader(args.src_file.as_ref())?;
+    let reader = reader(args.src_file.as_ref())?;
 
-    let mut line = String::new();
-    reader.read_line(&mut line)?;
-
-    let numbers = line.split(",").map(|s| s.trim().parse().unwrap()).collect();
+    let mut lines = Vec::new();
+    for line in reader.lines() {
+        let line = line?;
+        let line = Line::from_str(&line)?;
+        lines.push(line);
+    }
+    let lines = lines;
 
     let result = if args.part == 1 {
-        part1(numbers)
+        part1(lines)
     } else {
-        part2(numbers)
+        part2(lines)
     }?;
 
     info!("{:?}", result);
@@ -22,10 +27,46 @@ pub fn day8(args: &Arguments) -> Result<()> {
     Ok(())
 }
 
-fn part1(numbers: Vec<u64>) -> Result<usize> {
-    Ok(0)
+#[derive(Debug, Default)]
+struct Line {
+    left: Vec<String>,
+    right: Vec<String>,
 }
 
-fn part2(numbers: Vec<u64>) -> Result<usize> {
+impl Line {
+    fn from_str(s: &str) -> Result<Line> {
+        let mut parts: Vec<Vec<String>> = s
+            .split("|")
+            .map(|s| {
+                s.split(" ")
+                    .map(|s| s.trim())
+                    .filter(|s| !s.is_empty())
+                    .map(|s| s.to_owned())
+                    .collect()
+            })
+            .collect();
+
+        let line = Line {
+            left: parts.remove(0),
+            right: parts.remove(0),
+        };
+
+        Ok(line)
+    }
+}
+
+fn part1(lines: Vec<Line>) -> Result<usize> {
+    let easy_nums: HashSet<usize> = HashSet::from([2, 3, 4, 7]);
+
+    let result = lines
+        .into_iter()
+        .flat_map(|line| line.right)
+        .filter(|number| easy_nums.contains(&number.len()))
+        .count();
+
+    Ok(result)
+}
+
+fn part2(lines: Vec<Line>) -> Result<usize> {
     Ok(0)
 }

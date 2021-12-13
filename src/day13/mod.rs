da0ee1e103f627e24c3978c4aaeb6dbf7e253c23
Spencer Ferris <spencer@spencewenski.com>
2021-12-13 00:57:45 -0800

    Read input for day 13

--- src/day13/mod.rs
+++ src/day13/mod.rs
@@ -1,22 +1,34 @@
 use crate::arguments::Arguments;
 use crate::io::reader;
 use anyhow::Result;
+use itertools::Itertools;
 use std::io::BufRead;
 
 pub fn day13(args: &Arguments) -> Result<()> {
     let reader = reader(args.src_file.as_ref())?;
 
-    let mut lines = Vec::new();
+    let mut section = 0;
+    let mut positions = Vec::new();
+    let mut folds = Vec::new();
     for line in reader.lines() {
         let line = line?;
-        lines.push(line);
+        if line.trim().is_empty() {
+            section += 1;
+            continue;
+        }
+        if section == 0 {
+            positions.push(Position::from_line(line));
+        } else {
+            folds.push(Fold::from_line(line)?)
+        }
     }
-    let lines = lines;
+    let positions = positions;
+    let folds = folds;
 
     let result = if args.part == 1 {
-        part1(lines)
+        part1(positions, folds)
     } else {
-        part2(lines)
+        part2(positions, folds)
     }?;
 
     info!("{:?}", result);
@@ -24,10 +36,53 @@ pub fn day13(args: &Arguments) -> Result<()> {
     Ok(())
 }
 
-fn part1(lines: Vec<String>) -> Result<usize> {
+#[derive(Debug, Default)]
+struct Position {
+    x: usize,
+    y: usize,
+}
+
+impl Position {
+    fn new(x: usize, y: usize) -> Position {
+        Position { x, y }
+    }
+
+    fn from_line(line: String) -> Position {
+        let mut parts = line
+            .split(",")
+            .into_iter()
+            .map(|s| s.parse().unwrap())
+            .collect::<Vec<usize>>();
+
+        Position {
+            x: parts.remove(0),
+            y: parts.remove(0),
+        }
+    }
+}
+
+enum Fold {
+    Vertical(usize),
+    Horizontal(usize),
+}
+
+impl Fold {
+    fn from_line(line: String) -> Result<Fold> {
+        let line = line.replace("fold along ", "");
+        let parts = line.split("=").into_iter().collect_vec();
+
+        match *parts.get(0).unwrap() {
+            "x" => Ok(Fold::Vertical(parts.get(1).unwrap().parse().unwrap())),
+            "y" => Ok(Fold::Horizontal(parts.get(1).unwrap().parse().unwrap())),
+            _ => Err(anyhow::Error::msg("Invalid fold")),
+        }
+    }
+}
+
+fn part1(positions: Vec<Position>, folds: Vec<Fold>) -> Result<usize> {
     Ok(0)
 }
 
-fn part2(lines: Vec<String>) -> Result<usize> {
+fn part2(positions: Vec<Position>, folds: Vec<Fold>) -> Result<usize> {
     Ok(0)
 }
